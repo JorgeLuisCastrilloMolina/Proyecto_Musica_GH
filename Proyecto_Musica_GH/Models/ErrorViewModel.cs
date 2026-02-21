985f7e5a6b545388c7c2e28407d0b2784a84d798
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-02-21 02:52:25 -0600

    Initial commit

---
+++ Proyecto_Musica_GH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Musica_GH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
