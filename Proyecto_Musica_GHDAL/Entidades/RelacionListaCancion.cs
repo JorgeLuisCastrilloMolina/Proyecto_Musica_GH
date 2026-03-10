8e1b3c114abf9aa5c28e84f2d751fbb55eee0356
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-03-10 14:58:20 -0600

    Entidades Cancion / Playlist / RelacionListaCancion
    Definición de las entidades , en la capa DAL  , con los atributos correspondientes

---
+++ Proyecto_Musica_GHDAL/Entidades/RelacionListaCancion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Musica_GHDAL.Entidades
+{
+    public partial class RelacionListaCancion
+    {
+        public int LCRelId { get; set; }
+        public int PlaylistId { get; set; } // FK hacia Playlist
+        public int CancionId { get; set; }  // FK hacia Cancion
+        public int? Orden { get; set; }     // Puede ser nulo si no se define orden
+
+        // Propiedades de navegación opcionales
+        public Playlist? Playlist { get; set; }
+        public Cancion? Cancion { get; set; }
+    }
+
+}
