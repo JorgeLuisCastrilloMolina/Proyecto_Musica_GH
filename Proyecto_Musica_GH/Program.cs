a46e6c5aa3e22b64876050141fb27a0e7d3136aa
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-03-13 14:16:56 -0600

    Program modificado

--- Proyecto_Musica_GH/Program.cs
+++ Proyecto_Musica_GH/Program.cs
@@ -1,21 +1,46 @@
+using Proyecto_Musica_GHBLL;
+using Proyecto_Musica_GHBLL.Servicios.Cancion;
+using Proyecto_Musica_GHBLL.Servicios.Playlist;
+using Proyecto_Musica_GHBLL.Servicios.RelacionListaCancion;
+using Proyecto_Musica_GHDAL.Data;
+using Proyecto_Musica_GHDAL.Repositorios.Cancion;
+using Proyecto_Musica_GHDAL.Repositorios.Playlist;
+using Proyecto_Musica_GHDAL.Repositorios.RelacionListaCancion;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configuración de DbContext con SQLite
+builder.Services.AddDbContext<Proyecto_Musica_GHDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositorios y servicios de Canción
+builder.Services.AddScoped<ICancionRepositorio, CancionRepositorio>();
+builder.Services.AddScoped<ICancionServicio, CancionServicio>();
+
+// Repositorios y servicios de Playlist
+builder.Services.AddScoped<IPlaylistRepositorio, PlaylistRepositorio>();
+builder.Services.AddScoped<IPlaylistServicio, PlaylistServicio>();
+
+// Repositorios y servicios de RelacionListaCancion
+builder.Services.AddScoped<IRelacionListaCancionRepositorio, RelacionListaCancionRepositorio>();
+builder.Services.AddScoped<IRelacionListaCancionServicio, RelacionListaCancionServicio>();
+
+// AutoMapper con perfil de mapeo
+builder.Services.AddAutoMapper(cfg => { }, typeof(MapeoClases));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -24,6 +49,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
