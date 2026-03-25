82752973fe29df180dd4d9df20448239bbee5cb3
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-03-25 00:55:20 -0600

    .

--- Proyecto_Musica_GH/Controllers/ReproductorController.cs
+++ Proyecto_Musica_GH/Controllers/ReproductorController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*using Microsoft.AspNetCore.Mvc;
 using Proyecto_Musica_GHBLL.Servicios.Reproductor;
 using System.Threading.Tasks;
 
@@ -24,6 +24,84 @@ namespace Proyecto_Musica_GH.Controllers
         public async Task<IActionResult> CancionActual()
         {
             var response = await _reproductorServicio.ObtenerCancionActualAsync();
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Siguiente()
+        {
+            var response = await _reproductorServicio.CancionSiguienteAsync();
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Previa()
+        {
+            var response = await _reproductorServicio.CancionPreviaAsync();
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Play()
+        {
+            var response = await _reproductorServicio.PlayAsync();
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Detener()
+        {
+            var response = await _reproductorServicio.DetenerAsync();
+            return Json(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SeleccionarCancion(int id)
+        {
+            var response = await _reproductorServicio.SeleccionarCancionAsync(id);
+            return Json(response);
+        }
+    }
+}
+*/
+
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Musica_GHBLL.Servicios.Reproductor;
+using System.Threading.Tasks;
+
+namespace Proyecto_Musica_GH.Controllers
+{
+    public class ReproductorController : Controller
+    {
+        private readonly IReproductorServicio _reproductorServicio;
+
+        public ReproductorController(IReproductorServicio reproductorServicio)
+        {
+            _reproductorServicio = reproductorServicio;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CargarCanciones()
+        {
+            var response = await _reproductorServicio.CargarCancionesAsync();
+            return Json(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CancionActual()
+        {
+            var response = await _reproductorServicio.ObtenerCancionActualAsync();
+            return Json(response);
+        }
+
+        // NUEVO: devuelve todas las canciones
+        [HttpGet]
+        public async Task<IActionResult> Listar()
+        {
+            var response = await _reproductorServicio.ObtenerTodasCancionesAsync();
             return Json(response);
         }
 
