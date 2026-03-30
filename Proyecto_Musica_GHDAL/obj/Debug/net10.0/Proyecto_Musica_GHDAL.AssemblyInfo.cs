1d1910a34f90e43204445aa3385c3ef13ccd30ee
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-03-29 23:36:07 -0600

    .

--- Proyecto_Musica_GHDAL/obj/Debug/net10.0/Proyecto_Musica_GHDAL.AssemblyInfo.cs
+++ Proyecto_Musica_GHDAL/obj/Debug/net10.0/Proyecto_Musica_GHDAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Musica_GHDAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33bc108d6aa88493d25942fd5d5395ddabb05be2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8f8946444feb33dfa05ce5552196708ff50405")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Musica_GHDAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Musica_GHDAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
