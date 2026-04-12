55dde6ad64b34ccf25bde3e24e27c5ae4f22e5a8
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-04-12 12:00:10 -0600

    Login

---
+++ Proyecto_Musica_GH/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Proyecto_Musica_GH.Models
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Proyecto_Musica_GH/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Musica_GH.Models
+{
+    public class Usuario
+    {
+        public int Usuario_ID { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
