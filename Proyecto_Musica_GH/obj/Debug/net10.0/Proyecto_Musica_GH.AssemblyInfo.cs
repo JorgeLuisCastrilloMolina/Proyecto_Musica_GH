de25ba8e82837c15b938e57af21562b947dcd349
Jorge Luis Castrillo Molina <cjorge20752@ufide.ac.cr>
2026-04-13 16:58:01 -0600

    .

--- Proyecto_Musica_GH/obj/Debug/net10.0/Proyecto_Musica_GH.AssemblyInfo.cs
+++ Proyecto_Musica_GH/obj/Debug/net10.0/Proyecto_Musica_GH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Musica_GH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7461573c2a2adea2d5f0a4fde334a6a32c5d3fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd4824d068a4c45450d2e71f2b0fec3fe304cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Musica_GH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Musica_GH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
